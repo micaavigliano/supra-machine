c473c577486374c64eea3ab26f097dc93da09f56
Mica Avigliano <micaela.avigliano@gmail.com>
2024-11-07 21:47:28 -0300

    create dev environment

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,28 @@
 import client from "@/lib/contentful";
 
 const getBlogEntries = async() => {
-  const entries = await client.getEntries({ content_type: 'title' })
-  return entries
+  const entries = await client.getEntries({ content_type: 'title' });
+  return entries;
 }
 
 export default async function Home() {
-  const blogEntries = await getBlogEntries()
-  console.log(blogEntries)
-  return (<>
-    <h1>testing</h1>
-    {blogEntries.items.map(blog => {
-      return (
-        <div>
+  const blogEntries = await getBlogEntries();
+
+  return (
+    <>
+      <h1>Testing</h1>
+      {blogEntries.items.map((blog, index) => (
+        <div key={index}>
           <h1>{blog.fields.title}</h1>
-          <img src={blog.fields.images.fields.file.url} />
+          {Array.isArray(blog.fields.images) && blog.fields.images?.map((img, imgIndex) => (
+            <img
+              key={imgIndex}
+              src={img.fields.file.url}
+              alt={img.fields.title || 'Image'}
+            />
+          ))}
         </div>
-      )
-    })}
+      ))}
     </>
   );
 }
